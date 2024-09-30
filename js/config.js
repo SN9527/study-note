344194819cf2d5ccc7711fc55baca155b0930096
chenjiaqiang <1392945050@qq.com>
2024-09-30 09:47:01 +0800

    test

--- js/config.js
+++ js/config.js
@@ -1,3 +1,18 @@
+marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+});
+
+function myPlugin() {
+    console.log('loading test plugin');
+}
+
 var config = {
     elementId: 'cms',
     mode: 'GITHUB',
@@ -17,5 +32,30 @@ var config = {
     types: [
         {name: 'page', layout: { list: 'page-list', single: 'page' }},
     ],
+
+    plugins: [
+        myPlugin,
+    ],
+    markdownEngine: marked,
+
+    onload: function() {
+        console.log('cms.js ready');
+
+        document.getElementById('sort').addEventListener('click', function() {
+            var sortFunc = function(a, b) {
+                if(a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+                if(a.title.toLowerCase() > b.title.toLowerCase()) return 1;
+                return 0;
+            }
+            blog.sort('posts', sortFunc);
+        });
+
+        document.getElementById('search').addEventListener('input', function(e) {
+            blog.search('posts', 'title', e.target.value);
+        });
+
+        // Access the loaded plugins like this.
+        // blog.myPlugin();
+    },    
 };
-new CMS(config);
+var blog = new CMS(config);
